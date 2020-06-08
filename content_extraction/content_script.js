315e60c088d2ed63d00b68a077272574a76f8f30
Sherlock Wisdom <wisdomnji@gmail.com>
2020-06-08 10:31:00 +0100

    extracts components at depth

--- content_extraction/content_script.js
+++ content_extraction/content_script.js
@@ -3,8 +3,24 @@
 // document.body.style.backgroundColor = "black"
 window.oncontextmenu = process
 
+var depth = -1
+function checks( element ) {
+	++depth
+	console.log("At depth:", depth)
+	const child_element_count = element.childElementCount
+	const class_name = element.className
+	const node_name = element.nodeName
+	const node_type = element.nodeType
+	console.log("child_element_count:", length)
+	console.log("classname:", class_name)
+	console.log("node_name:", node_name)
+	console.log("node_type:", node_type)
+	checks( element.parentNode )
+	console.log()
+}
 
 function process( click_event ) {
+	/*
 	let component = click_event.originalTarget
 	// console.log(component.firstChild)
 	console.log(component.className)
@@ -12,4 +28,19 @@ function process( click_event ) {
 	for( let i in contents ) {
 		contents[i].innerHTML = "Go fuck yourself!"
 	}
+	*/
+
+	const active_component = document.activeElement
+	console.log(active_component)
+	// childElementCount
+	//
+	// when matching element is reached, parent is parsed for all other occurences
+	// find items with matching attributes, first by length, then by content
+	// then check against childElementCount
+	// then check against className
+	// then check against nodeName
+	// then check against nodeType
+	// maybe? check same elements against parentNode
+	
+	checks( active_component )
 }
