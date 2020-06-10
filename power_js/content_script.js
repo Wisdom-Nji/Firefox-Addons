c7ae34d15721bf464adc06b4ff28258323925071
Sherlock Wisdom <wisdomnji@gmail.com>
2020-06-10 14:56:28 +0100

    recursively extracts contents from webpages

---
+++ power_js/content_script.js
@@ -0,0 +1,35 @@
+// console.log(document.body.children)
+
+const ch_body = document.body.children
+recursive_extract( ch_body )
+
+function element_info( element ) {
+	const id = element.id
+	const type = element.nodeType
+	const node_name = element.nodeName
+	const number_children = element.childElementCount
+
+	// console.log(element)
+	console.log("element_id:", id)
+	console.log("type:", type)
+	console.log("node_name:", node_name)
+	console.log("# children:", number_children)
+}
+
+function recursive_extract( element ) {
+	for( let i in element ) {
+		let child = element[i]
+		if( is_element( child ) == false) continue
+		console.log(`Working=>[${child.nodeName}]`)
+		element_info( child )
+
+		if( child.childElementCount > 0) 
+			recursive_extract( child.children )
+		console.log(`Done=>[${child.nodeName}]`)
+	}
+	console.log()
+}
+
+function is_element( element ) {
+	return !(typeof element.nodeName == "undefined")
+}
