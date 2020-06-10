f15e873552a79cca25f7ac7d3f0285a53132147b
Sherlock <wisdomnji@gmail.com>
2020-06-10 22:09:00 +0100

    attribute rules added

--- power_js/js_parser.js
+++ power_js/js_parser.js
@@ -27,17 +27,25 @@ function parser( html ) {
 	console.log("tag_space:", tag_space)
 
 	let split_tag = tag_space.split(' ')
+	// attributes should not have space between the =
 	console.log("split_tag:", split_tag)
 	let tag = split_tag[0]
 	console.log("tag:", tag)
 	let atr_space = split_tag[1]
 	console.log("attributes:", atr_space)
 	let split_atr = atr_space.split("=")
+	// split_atr should be extracted in a loop because could be > 1 attribute
 	console.log("split_atr:", split_atr)
+	let attr = split_atr[0]
+	let attr_values = split_atr[1]
 	
 	// this is where the conditionals come in. a map of all the elements and their attributes would be needed here
+	let values = {
+		tag : tag,
+		attr : [[attr, attr_values]]
+	}
 
-	return document.createElement( tag.toUpperCase() )
+	return values
 }
 
 
