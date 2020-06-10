b0a4c4d447725ec1c99e51df30bfefc0907ae23c
Sherlock <wisdomnji@gmail.com>
2020-06-10 21:25:14 +0100

    sample files added

---
+++ power_js/js_parser.js
@@ -0,0 +1,14 @@
+const sample_html = "<html><head></head><body><p>p1</p><div><p>p2</p></div></body></html>"
+const single_tag = "<p>"
+const single_tag_false = "<p"
+const single_element = "<img src=\"..\" >"
+
+
+function is_element( element ) {
+}
+
+function element_count( html ) {
+}
+
+function parser( html ) {
+}
